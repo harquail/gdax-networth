ec268fdc0cd89c898647043073a861ceeee64679
harquail <nook@harquail.com>
2017-06-25 16:16:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Simple script that prints the total value of all cryptocurrency holdings in a GDAX account in a base fiat currency.  
+
+It reads [GDAX API keys](https://support.gdax.com/customer/en/portal/articles/2425383-how-can-i-create-an-api-key-for-gdax-) from 'readFromGdaxKey.json' to authenticate.
+
+To print the value in a different currency, change the BASE_CURRENCY constant.
+
+=== Example use case ===
+To save net worth history to a log file, one might set up a cron job that runs ```node printNetWorth.js >> accountHistory.txt``` periodically.
+ 
---
+++ printNetWorth.js
@@ -0,0 +1,85 @@
+const Gdax = require('gdax');
+const BASE_CURRENCY = 'USD';
+
+/** @const {key:string, secret:string, pass:string} */
+const credentials = require('./readFromGdaxKey.json');
+
+const productsPromise = new Promise((resolve, reject) => {
+    const publicClient = new Gdax.PublicClient();
+    publicClient.getProducts((err, response, data) => {
+        if (err || response.statusCode > 300) {
+            reject(data);
+        }
+        else {
+            resolve(data);
+        }
+    });
+});
+
+
+/**
+ * @param {string} product 
+ * @return {Promise<{product: string, price: number}>}
+ */
+function pricePromiseFactory(product) {
+    return new Promise((resolve, reject) => {
+        const publicClient = new Gdax.PublicClient(product);
+        publicClient.getProductTicker((err, response, data) => {
+            if (err || response.statusCode > 300) {
+                reject(data);
+            }
+            else if (data) {
+                resolve({ product: product, price: data.price });
+            }
+        });
+    });
+}
+
+const accountHoldingsPromise = new Promise((resolve, reject) => {
+    const authedClient = new Gdax.AuthenticatedClient(credentials.key, credentials.secret, credentials.pass);
+    authedClient.getAccounts((err, response, data) => {
+        if (err || response.statusCode > 300) {
+            reject(data);
+        }
+        else {
+            resolve(data);
+        }
+    });
+});
+
+
+/// print net worth by currency
+console.log(`\n${new Date().toISOString()}\n GDAX net worth in ${BASE_CURRENCY}:`);
+productsPromise.then(
+    (products) => {
+        const toFiatProducts = products.filter((product) => { return product.quote_currency === BASE_CURRENCY })
+        const toFiatPromises = toFiatProducts.map((p) => {
+            return pricePromiseFactory(p.id)
+        });
+
+        // wait for the account and all prices to return
+        const allPromises = [accountHoldingsPromise].concat(toFiatPromises);
+        Promise.all(allPromises).then((data) => {
+            const accountHoldings = data.shift();
+            const currencyValues = data;
+            // will hold source currency values in base currency
+            let holdings = {};
+            for (holding of accountHoldings) {
+                if (holding.currency === BASE_CURRENCY) {
+                    holdings[holding.currency] = Number(holding.balance);
+                }
+                else {
+                    // get value in base currency of each source currency
+                    const matchingCurrency = currencyValues.find((c) => {
+                        return (c.product === `${holding.currency}-${BASE_CURRENCY}`);
+                    })
+                    const value = holding.balance * matchingCurrency.price;
+                    holdings[holding.currency] = value;
+                }
+            }
+            console.log(holdings);
+            // sum all currencies
+            let total = Object.keys(holdings).reduce((p, c, i) => { return p + holdings[c] }, 0);
+            console.log(total);
+        });
+    });
